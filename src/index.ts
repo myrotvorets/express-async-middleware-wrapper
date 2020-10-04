543fa7e565fb36ec11c4fb4724702ed586f5e649
Myrotvorets <support@myrotvorets.center>
2020-10-05 02:40:07 +0300

    Fix for @opentelemetry/plugin-express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import type { NextFunction, Request, RequestHandler, Response } from 'express';
 
 export default function asyncWrapperMiddleware(fn: RequestHandler): RequestHandler {
-    return (req: Request, res: Response, next: NextFunction): void => {
-        Promise.resolve(fn(req, res, next)).catch(next);
+    const name = `async wrapper for ${fn.name}`;
+    const r = {
+        [name]: function (req: Request, res: Response, next: NextFunction): void {
+            Promise.resolve(fn(req, res, next)).catch(next);
+        },
     };
+
+    return r[name];
 }
