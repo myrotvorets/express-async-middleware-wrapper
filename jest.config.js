f853bd02f9ea4a99f00b2d9e4c06dc46e717fc4f
Myrotvorets <support@myrotvorets.center>
2020-10-19 08:53:17 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,10 @@
 const merge = require('merge');
 const ts_preset = require('ts-jest/jest-preset');
 
-if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'test') {
-    process.env.NODE_ENV = 'test';
-    process.env.KNEX_DRIVER = 'sqlite3';
-    process.env.KNEX_DATABASE = ':memory:';
-}
-
 module.exports = merge.recursive(ts_preset, {
     collectCoverage: true,
     collectCoverageFrom: [
         'src/**/*.ts',
-        '!src/migrations/**',
         '!src/test/**',
     ],
     clearMocks: true,
