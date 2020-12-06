eeef6a4394b49fa7dccd01665776894fcb1b64b9
Myrotvorets <support@myrotvorets.center>
2020-12-06 07:27:54 +0200

    Major refactoring

---
+++ lib/index.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { NextFunction, Request, RequestHandler, Response } from 'express';
+// eslint-disable-next-line import/no-unresolved
+import type core from 'express-serve-static-core';
+
+type RequestHandlerParams<R, Res, Req, Query> = Parameters<RequestHandler<R, Res, Req, Query>>;
+
+export type AsyncRequestHandler<P = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = core.Query> = (
+    ...args: RequestHandlerParams<P, ResBody, ReqBody, ReqQuery>
+) => Promise<void>;
+
+export default function asyncWrapperMiddleware<
+    P = core.ParamsDictionary,
+    ResBody = any,
+    ReqBody = any,
+    ReqQuery = core.Query
+>(
+    fn: RequestHandler<P, ResBody, ReqBody, ReqQuery> | AsyncRequestHandler<P, ResBody, ReqBody, ReqQuery>,
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> {
+    const name = `async wrapper for ${fn.name}`;
+    const r = {
+        [name]: function (
+            req: Request<P, ResBody, ReqBody, ReqQuery>,
+            res: Response<ResBody>,
+            next: NextFunction,
+        ): void {
+            Promise.resolve(fn(req, res, next)).catch(next);
+        },
+    };
+
+    return r[name];
+}
